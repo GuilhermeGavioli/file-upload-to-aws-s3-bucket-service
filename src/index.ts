b175a17f971532fd87eca8c4e05b89254fa61c70
Guilherme Gavioli <xiiguilhermeiix@gmail.com>
2022-12-21 01:35:04 -0300

    set up express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { StorageClient } from "./StorageStrategy/StorageClient";
 
 console.log('hello')
+
+import express from 'express'
+const app = express();
+
+
+app.get('/', (req, res) => { 
+    res.send('oklok');
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
